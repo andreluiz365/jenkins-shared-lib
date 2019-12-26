ba3e1ca848334c7612404e8ce764b04af4af7b75
andreluiz <andreluizvianna@outlook.com>
2019-12-26 16:03:25 -0300

    ajustes lib

--- vars/printBuildinfo.groovy
+++ vars/printBuildinfo.groovy
@@ -9,7 +9,7 @@ def call(body) {
     echo config.name
     echo "Param0 is: ${env.param0}"
     echo "Param1 is: ${env.param1}"
-    echo "Param2 is: ${env.oarms2}
+    echo "Param2 is: ${env.oarms2}"
     if (env.param0 == 'One default') {
         echo "Param0 is default"
     }
